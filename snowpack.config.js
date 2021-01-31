e90ebccc8e4823eb679101efde4009f4acd4b709
aldaprojects <aldaprojects@hotmail.com>
2021-01-31 13:34:06 -0600

    template

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-postcss',
   ],
   routes: [
     /* Example: Enable an SPA Fallback in development: */
